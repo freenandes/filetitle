f321d28c04624fa9dc7163bf9c371fff721f6470
pmcf-percona <uuuuurso@gmail.com>
2025-05-12 11:20:44 +0100

    chore: initial commit of frontmatter-filename plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,43 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+export default class FrontmatterFilenamePlugin extends Plugin {
+	onload() {
+		this.registerEvent(
+			this.app.vault.on('modify', (file) => {
+				if (!(file instanceof TFile) || file.extension !== 'md') return;
+				this.renameOnSave(file);
+			})
+		);
 	}
 
-	onunload() {
+	private async renameOnSave(file: TFile) {
+		const content = await this.app.vault.read(file);
 
-	}
+		const fmMatch = content.match(/^---\s*([\s\S]*?)\s*---/);
+		if (!fmMatch) return;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+		const titleMatch = fmMatch[1].match(/^\s*title\s*:\s*(?:["']?)(.+?)(?:["']?)\s*$/m);
+		if (!titleMatch) return;
+		const title = titleMatch[1];
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+		const safeName = title
+			.normalize('NFD')                  // decompose accents
+			.replace(/[\u0300-\u036f]/g, '')   // remove diacritics
+			.toLowerCase()                     // lowercase
+			.replace(/[^a-z0-9\s-]/g, '')      // drop invalid chars
+			.trim()                            // trim edges
+			.replace(/\s+/g, '-')              // spaces → hyphens
+			.replace(/-+/g, '-')               // collapse multi-hyphens
+			.replace(/^-+|-+$/g, '');          // trim stray hyphens
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		const newFilename = `${safeName}.md`;
+		if (file.name === newFilename) return;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const newPath = file.path.replace(/[^/]+$/, newFilename);
+		try {
+			await this.app.fileManager.renameFile(file, newPath);
+		} catch (e) {
+			console.error('FrontmatterFilename › rename failed', e);
+		}
 	}
 }
